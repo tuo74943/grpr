f4405b1d6802af97798331c4ea55335edb785c16
Rosalio Gutierrez <Tuo74943@temple.edu>
2022-02-09 01:50:11 -0500

    Created working login/registration

--- app/src/main/java/edu/temple/grpr/LoginOrRegister.kt
+++ app/src/main/java/edu/temple/grpr/LoginOrRegister.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,19 +19,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class LoginOrRegister : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
     lateinit var loginButton : Button
     lateinit var registerButton: Button
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var userNameEditText: EditText
+    lateinit var passwordEditText: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,6 +32,8 @@ class LoginOrRegister : Fragment() {
         val layout = inflater.inflate(R.layout.fragment_login_or_register, container, false)
         loginButton = layout.findViewById(R.id.loginButton)
         registerButton = layout.findViewById(R.id.registerButton)
+        userNameEditText = layout.findViewById(R.id.usernameEditText)
+        passwordEditText = layout.findViewById(R.id.passwordEditText)
         return layout
     }
 
@@ -47,35 +41,16 @@ class LoginOrRegister : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         loginButton.setOnClickListener {
-            (activity as loginOrRegisterInterface).loginButtonPressed();
+            (activity as loginOrRegisterInterface).loginButtonPressed(userNameEditText.text.toString(), passwordEditText.text.toString());
         }
 
         registerButton.setOnClickListener {
             (activity as loginOrRegisterInterface).registerButtonPressed()
         }
     }
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginOrRegister.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginOrRegister().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 
     interface loginOrRegisterInterface{
-        fun loginButtonPressed()
+        fun loginButtonPressed(username : String, password : String)
         fun registerButtonPressed()
     }
 }
--- app/src/main/java/edu/temple/grpr/MainActivity.kt
+++ app/src/main/java/edu/temple/grpr/MainActivity.kt
@@ -4,11 +4,32 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
-lateinit var registerIntent : Intent
 
 class MainActivity : AppCompatActivity(), LoginOrRegister.loginOrRegisterInterface{
+
+    lateinit var registerIntent : Intent
+
+    companion object {
+        const val loginAPI = "https://kamorris.com/lab/grpr/account.php"
+    }
+
+    val volleyQueue : RequestQueue by lazy {
+        Volley.newRequestQueue(this)
+    }
+
+    val mPrefs by lazy {
+        getPreferences(MODE_PRIVATE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,24 +37,85 @@ class MainActivity : AppCompatActivity(), LoginOrRegister.loginOrRegisterInterfa
         registerIntent = Intent(this, RegisterActivity::class.java)
 
         if(savedInstanceState == null){
-            supportFragmentManager.beginTransaction()
-                .add(R.id.container, LoginOrRegister())
-                .commit()
+            if(mPrefs.contains("session_key")){
+
+                Log.d("MainFragment", "what " + mPrefs.contains("username"))
+                Log.d("MainFragment", "session_key : " + mPrefs.getString("session_key", null))
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.container, MainFragment())
+                    .commit()
+            }
+            else{
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.container, LoginOrRegister())
+                    .commit()
+            }
         }
 
 
 
     }
 
-    override fun loginButtonPressed() {
+    override fun loginButtonPressed(username : String, password: String) {
         Toast.makeText(this, "login button pressed", Toast.LENGTH_LONG).show()
         Log.d("login button", "user clicked login button")
         //TODO send login request to API
+        loginRequest(username, password)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment())
+            .commit()
     }
 
     override fun registerButtonPressed() {
         Toast.makeText(this, "register button pressed", Toast.LENGTH_LONG).show()
         Log.d("resgister button", "user clicked register button")
         startActivity(registerIntent)
+    }
+
+    fun loginRequest(username: String, password: String){
+        val stringRequest: StringRequest = object : StringRequest(
+            Method.POST,
+            loginAPI,
+            Response.Listener { response ->
+                Log.i("Response", response.toString())
+                val jsonObj = JSONObject(response)
+                if(jsonObj.getString("status") == "ERROR"){
+                    findViewById<TextView>(R.id.textView6).text = jsonObj.getString("message")
+                }
+                else{
+                    val editor = mPrefs.edit()
+                    editor.putString("session_key", jsonObj.getString("session_key"))
+                        .putString("username", username)
+                        .apply()
+                    Toast.makeText(this,"Login Complete", Toast.LENGTH_SHORT).show()
+                }
+            },
+            Response.ErrorListener { error ->
+                Log.i("response error:", "$error")
+                error.printStackTrace()
+            }){
+            override fun getBodyContentType(): String {
+                return "application/x-www-form-urlencoded"
+            }
+
+            override fun getHeaders(): Map<String, String> {
+                val headers: MutableMap<String, String> =
+                    HashMap()
+                headers["Content-Type"] = "application/x-www-form-urlencoded"
+                headers["Accept"] = "application/json"
+                return headers
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getParams(): Map<String, String> {
+                val params: MutableMap<String, String> =
+                    HashMap()
+                params["action"] = "LOGIN"
+                params["username"] = username
+                params["password"] = password
+                return params
+            }
+        }
+        volleyQueue.add(stringRequest)
     }
 }
