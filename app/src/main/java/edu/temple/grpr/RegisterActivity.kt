acffa498511a797c98165f275d7567f39d23ecf3
Rosalio Gutierrez <Tuo74943@temple.edu>
2022-02-08 21:58:56 -0500

    Register Activity now references layout and when user clicks button runs a function. Should execute a query request but left TODO

--- app/src/main/java/edu/temple/grpr/RegisterActivity.kt
+++ app/src/main/java/edu/temple/grpr/RegisterActivity.kt
@@ -2,10 +2,40 @@ package edu.temple.grpr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
+    val usernameEditText by lazy {
+         findViewById<EditText>(R.id.usernameEditText)
+    }
+
+    val fNameEditText by lazy {
+        findViewById<EditText>(R.id.fNameEditText)
+    }
+
+    val lNameEditText by lazy {
+        findViewById<EditText>(R.id.lNameEditText)
+    }
+
+    val passwordEditText by lazy {
+        findViewById<EditText>(R.id.passwordEditText)
+    }
+
+    val button by lazy {
+        findViewById<Button>(R.id.button)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.register_activity)
+
+        button.setOnClickListener {
+            val userName = usernameEditText.text
+            val firstName = fNameEditText.text
+            val lastName = lNameEditText.text
+            val password = passwordEditText.text
+        }
+
     }
 }
