78f2be6b4b60cc3a941bf3534b2c932f22ed4dad
Rosalio Gutierrez <tuo74943@temple.edu>
2022-03-05 16:04:07 -0500

    added a fragment containing only information regarding the audio recording

---
+++ app/src/main/java/edu/temple/grpr/AudioListFragment.kt
@@ -0,0 +1,27 @@
+package edu.temple.grpr
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AudioListFragment : Fragment() {
+
+    lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val layout = inflater.inflate(R.layout.fragment_audio_list, container, false)
+
+        recyclerView = layout.findViewById(R.id.recyclerView)
+        //TODO add an adapter and layout manager
+
+        return layout
+    }
+
+}
